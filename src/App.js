14c79ca54e3cf629261adf5674624784c672afbd
shofol <18103156@iubat.edu>
2022-03-24 02:35:42 +0600

    component structure created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./Components/Header/Header";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Products from "./Components/Products/Products";
+import Footer from "./Shared/Footer/Footer";
+import Register from "./Components/Register/Register";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Header></Header>
+        <Routes>
+          <Route path="/" element={<Products />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+        <Footer></Footer>
+      </Router>
     </div>
   );
 }
