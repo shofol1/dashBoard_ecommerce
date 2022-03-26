f0736a83344925cd7ef783a783d0ffd2bf74174c
shofol <18103156@iubat.edu>
2022-03-26 22:17:38 +0600

    private route creation using outlet react dom, set data to local storage ,clear data from local storage

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Products from "./Components/Products/Products";
 import Footer from "./Shared/Footer/Footer";
 import Register from "./Components/Register/Register";
+import PrivateRoute from "./Components/PrivateRoute/PrivateRoute";
 
 function App() {
   return (
@@ -11,7 +12,12 @@ function App() {
       <Router>
         <Header></Header>
         <Routes>
-          <Route path="/" element={<Products />} />
+          <Route element={<PrivateRoute />}>
+            <Route path="/" element={<Products />} />
+            <Route path="/update" element={<h1>update page</h1>} />
+            <Route path="/add" element={<h1>add page</h1>} />
+            <Route path="/profile" element={<h1>profile page</h1>} />
+          </Route>
           <Route path="/register" element={<Register />} />
         </Routes>
         <Footer></Footer>
